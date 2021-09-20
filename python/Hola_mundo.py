ce041689e289ab5711dd96107cc109fc42fdb659
W4LT3R <walterdavila1000@gmail.com>
2021-09-20 05:17:16 -0600

    200921
    Signed-off-by: W4LT3R <walterdavila1000@gmail.com>

--- python/Hola_mundo.py
+++ python/Hola_mundo.py
@@ -1,2 +1,18 @@
 print("Hola mundo")
 print("Mucho gusto a los presentes.")
+"""
+Git: 
+git init -> Crear repositorio.
+git status -> Verifica el estado en el código.
+git add . -> Guardar en local todos los archivos <.>
+git commit -m "Nombre" -> Punto de restauración.
+git push -> Repositorio en línea.
+
+git pull -> Transferir cambios desde el repositorio en línea al local.
+
+git log -> Ver commits.
+git log --oneline -> Ver commits en forma resumida.
+git reset --hard <ID del commit>
+
+git branch
+"""
